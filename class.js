d53d238c8b93720e930726ea165d2d99e49691d1
mohsenam <mohsenam@mohsenams-MacBook-Pro.local>
2024-06-16 09:57:28 -0400

    functions

--- class.js
+++ class.js
@@ -1,13 +1,29 @@
 //classes is used for create a template of an object
+//100 html -> 1js -> 1css
+//repeativie code
+// manage change on code and logics
+
+//inheritance a-> b:a -> b==a
+//encapsulation
+//polymorphism
 class User {
   fName;
   lName;
   age;
+
+  #fullName;
   constructor(params) {}
 
   buildFullName() {
-    return this.fName + " " + this.lName;
+    this.fullName = this.fName + " " + this.lName;
+    return this.fullName;
   }
+
+  buildFullName(...x) {
+    console.log(x);
+  }
+  buildFullName(firstName, lastName) {}
+  buildFullName(fullName) {}
 }
 
 const myUser = new User();
@@ -15,6 +31,10 @@ myUser.age = 12;
 myUser.fName = "ahmad";
 myUser.lName = "ahmad poor";
 myUser.nationalId = 123123123;
+
+myUser.fullName = "full name";
 const fullName = myUser.buildFullName();
+myUser.buildFullName("firstName", "lastName");
+myUser.buildFullName("sdf", "sdfsdf", 111, 222, 333, 4444);
 console.log(myUser);
 console.log(fullName);
---
+++ scopes.js
@@ -0,0 +1,11 @@
+const a = "a";
+let b = "b";
+var c = "c";
+
+function declareVaiables() {}
+function myFunction() {
+  console.log(c);
+}
+console.log(c);
+
+myFunction();
